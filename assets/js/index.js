00f641e79e38cdfab534113c3997c7675906fa32
KaelDrifts <kaeldrifts@gmail.com>
2025-04-16 18:24:51 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 const open = document.getElementById("info_pop");
 const popup = document.getElementById("popup_container");
 const close = document.getElementById("popup_close");
-
+//read later
 open.addEventListener("click", () => {
     popup.classList.add("show");
 });
